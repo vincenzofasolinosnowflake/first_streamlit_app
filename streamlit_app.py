eb043d57c78cf454e1c1f3fd1c154560577972de
vincenzofasolinosnowflake <127206126+vincenzofasolinosnowflake@users.noreply.github.com>
2023-03-17 09:17:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,7 @@ if streamlit.button('add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
  
+ 
 streamlit.text(back_from_function)
   
 # Create therepeatable code block (called a function) : def
